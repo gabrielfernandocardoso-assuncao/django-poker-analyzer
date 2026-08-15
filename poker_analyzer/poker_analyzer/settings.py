d54ffb8f19e496534dfc1ce51547c1708550b5df
fernando <gabrielfcassuncao@gmail.com>
2026-08-15 20:47:19 -0300

    fiz o frontend

--- poker_analyzer/poker_analyzer/settings.py
+++ poker_analyzer/poker_analyzer/settings.py
@@ -28,7 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'games'
+    'games',
+    'crispy_forms',
+    'crispy_bootstrap5'
 ]
 
 MIDDLEWARE = [
@@ -107,3 +109,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5" 
+CRISPY_TEMPLATE_PACK = "bootstrap5"
